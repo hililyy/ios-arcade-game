000e7a702b59dac25aeed11da34aeb70ce9e1b9c
hililyy <joun406@gmail.com>
2023-07-11 18:22:40 +0900

    [FEAT] 필드 애니메이션 추가

---
+++ FlappyBirdLike/FlappyBirdLike/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  FlappyBirdLike
+//
+//  Created by 강조은 on 2023/07/11.
+//
+
+import Foundation
+
+struct Layer {
+    static let sky: CGFloat = 1
+    static let pipe: CGFloat = 2
+    static let land: CGFloat = 3
+    static let ceiling: CGFloat = 4
+    static let bird: CGFloat = 5
+}
